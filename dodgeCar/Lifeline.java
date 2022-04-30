b845f8e069e5db33d42b6edf30d05116c9c1fbcc
Haard Shah <haard.dravid@gmail.com>
2022-04-30 13:37:37 -0700

    Added base code

---
+++ dodgeCar/Lifeline.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Score here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+/**
+ * Write a description of class Score here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lifeline extends Actor
+{
+    private MyWorld world;
+    
+    public Lifeline(MyWorld world) {
+        this.world = world;
+    }
+    
+    /**
+     * Act - do whatever the Score wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        String s = "Level " + world.getLevel() + ", Score: " + world.getScore();
+        GreenfootImage img = new GreenfootImage(s, 24, Color.GREEN, Color.BLACK);
+        setImage(img);
+    }    
+}
+
